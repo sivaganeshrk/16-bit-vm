c2da5f842f867d587266e03fa6e33e05f102a36c
Siva Ganesh <sivadoog0808@gmail.com>
2022-12-18 22:12:52 +0530

    Added Instruction Executor

--- src/cpu.ts
+++ src/cpu.ts
@@ -1,5 +1,6 @@
 import { createMemory } from "./memory";
 import { DynamicObject } from "./types";
+import instructions from "./instructions";
 
 export class CPU {
   memory: DataView;
@@ -37,7 +38,6 @@ export class CPU {
     if (!(name in this.registerMap)) {
       throw new Error(`getRegister: No such register '${name}'`);
     }
-
     return this.registers.getUint16(this.registerMap[name]);
   }
 
@@ -45,7 +45,51 @@ export class CPU {
     if (!(name in this.registerMap)) {
       throw new Error(`getRegister: No such register '${name}'`);
     }
-
     return this.registers.setUint16(this.registerMap[name], value);
+  }
+
+  fetch() {
+    const nextInstructionAddress = this.getRegister("ip");
+    const instruction = this.memory.getUint8(nextInstructionAddress);
+    this.setRegister("ip", nextInstructionAddress + 1);
+    return instruction;
+  }
+
+  fetch16() {
+    const nextInstructionAddress = this.getRegister("ip");
+    const instruction = this.memory.getUint16(nextInstructionAddress);
+    this.setRegister("ip", nextInstructionAddress + 1);
+    return instruction;
+  }
+
+  execute(instruction: number) {
+    switch (instruction) {
+      // Move literal into the r1 register
+      case instructions.MOV_LIT_R1: {
+        const literal = this.fetch16();
+        this.setRegister("r1", literal);
+        return;
+      }
+      // Move literal into the r2 register
+      case instructions.MOV_LIT_R2: {
+        const literal = this.fetch16();
+        this.setRegister("r2", literal);
+        return;
+      }
+      // Add register to register
+      case instructions.ADD_REG_REG: {
+        const r1 = this.fetch();
+        const r2 = this.fetch();
+        const registerValue1 = this.registers.getUint16(r1 * 2);
+        const registerValue2 = this.registers.getUint16(r2 * 2);
+        this.setRegister("acc", registerValue1 + registerValue2);
+        return;
+      }
+    }
+  }
+
+  step() {
+    const instruction = this.fetch();
+    return this.execute(instruction);
   }
 }
