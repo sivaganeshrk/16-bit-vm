8dedc358a1fc31c9cdcb29d33e3fdf331bdabc6d
Siva Ganesh <sivadoog0808@gmail.com>
2023-01-15 21:15:10 +0530

    Refactor the instruction and added new instruction

--- src/instructions.ts
+++ src/instructions.ts
@@ -1,13 +1,8 @@
-const MOV_LIT_REG = 0x10;
-const MOV_REG_REG = 0x11;
-const MOV_REG_MEM = 0x12;
-const MOV_MEM_REG = 0x13;
-const ADD_REG_REG = 0x14;
-
 export = {
-  MOV_LIT_REG,
-  MOV_REG_REG,
-  MOV_REG_MEM,
-  MOV_MEM_REG,
-  ADD_REG_REG,
+  MOV_LIT_REG: 0x10,
+  MOV_REG_REG: 0x11,
+  MOV_REG_MEM: 0x12,
+  MOV_MEM_REG: 0x13,
+  ADD_REG_REG: 0x14,
+  JMP_NOT_EQ: 0x15,
 };
