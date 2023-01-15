d7b23ada63c3b4349a7e1f9324228a6a5c8e98fa
Siva Ganesh <sivadoog0808@gmail.com>
2023-01-15 22:26:18 +0530

    Added Sample Code for the simple JNE Branching

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { CPU } from "./cpu";
 import { createMemory } from "./memory";
 import instructions from "./instructions";
-
+import readline from "readline";
 const IP = 0;
 const ACC = 1;
 const R1 = 2;
@@ -13,45 +13,45 @@ const cpu = new CPU(memory);
 
 let i = 0;
 
-writableBytes[i++] = instructions.MOV_LIT_REG;
-writableBytes[i++] = 0x12; //0x1234
-writableBytes[i++] = 0x34;
+writableBytes[i++] = instructions.MOV_MEM_REG;
+writableBytes[i++] = 0x01; //0x0100
+writableBytes[i++] = 0x00;
 writableBytes[i++] = R1;
 
 writableBytes[i++] = instructions.MOV_LIT_REG;
-writableBytes[i++] = 0xab; //0xabcd
-writableBytes[i++] = 0xcd;
+writableBytes[i++] = 0x00; //0x0001
+writableBytes[i++] = 0x01;
 writableBytes[i++] = R2;
 
 writableBytes[i++] = instructions.ADD_REG_REG;
-writableBytes[i++] = R1; //r1 index
-writableBytes[i++] = R2; //r2 index
+writableBytes[i++] = R1;
+writableBytes[i++] = R2;
 
 writableBytes[i++] = instructions.MOV_REG_MEM;
 writableBytes[i++] = ACC;
 writableBytes[i++] = 0x01;
-writableBytes[i++] = 0x00; // 0x0100
+writableBytes[i++] = 0x00; //0x0100
 
+writableBytes[i++] = instructions.JMP_NOT_EQ;
+writableBytes[i++] = 0x00;
+writableBytes[i++] = 0x03;
+writableBytes[i++] = 0x00;
+writableBytes[i++] = 0x00;
+
+//Initial State
+console.log("Initial State");
 cpu.debug();
 cpu.viewMemoryAt(cpu.getRegister("ip"));
 cpu.viewMemoryAt(0x0100);
 
-cpu.step();
-cpu.debug();
-cpu.viewMemoryAt(cpu.getRegister("ip"));
-cpu.viewMemoryAt(0x0100);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-cpu.step();
-cpu.debug();
-cpu.viewMemoryAt(cpu.getRegister("ip"));
-cpu.viewMemoryAt(0x0100);
-
-cpu.step();
-cpu.debug();
-cpu.viewMemoryAt(cpu.getRegister("ip"));
-cpu.viewMemoryAt(0x0100);
-
-cpu.step();
-cpu.debug();
-cpu.viewMemoryAt(cpu.getRegister("ip"));
-cpu.viewMemoryAt(0x0100);
+rl.on("line", () => {
+  cpu.step();
+  cpu.debug();
+  cpu.viewMemoryAt(cpu.getRegister("ip"));
+  cpu.viewMemoryAt(0x0100);
+});
